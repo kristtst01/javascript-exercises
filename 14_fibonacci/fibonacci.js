a5c30b8f288e8f8d0896b2b76904e0bf957b99c3
Kristian Tufte Sterud <kristian0805@hotmail.com>
2025-08-20 17:44:01 +0200

    finished the rest of the exercises. FindTheOldest was solved with normal for of loop, not reduce.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (a) {
+  if (a < 0) {
+    return "OOPS";
+  }
+  if (a == 0) {
+    return 0;
+  } else if (a == 1) {
+    return 1;
+  } else {
+    return fibonacci(a - 1) + fibonacci(a - 2);
+  }
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
+const findTheOldest = function (people) {
+  let oldestPerson = null;
+  let oldestAge = 0;
+  for (let person of people) {
+    const age = getAge(person.yearOfBirth, person.yearOfDeath);
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = person;
+    }
+  }
+  return oldestPerson;
 };
 
 // Do not edit below this line
