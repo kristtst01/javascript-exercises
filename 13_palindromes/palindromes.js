72ee16d3d2abcb244edba3b3d3ed80205376e2bc
Kristian Tufte Sterud <kristian0805@hotmail.com>
2025-08-20 10:01:08 +0200

    finished 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const punctRemoved = remove(string).toLowerCase();
+  const reversed = Array.from(punctRemoved).reverse().join("").toLowerCase();
+  return reversed === punctRemoved;
 };
+
+function remove(str) {
+  return str.replace(/[!\s"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, "");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
